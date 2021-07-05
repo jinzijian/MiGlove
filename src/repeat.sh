1b5ab6ea3e1014b900f40644ec7bd19086633e4a
AlexKing <zijianjin0730@gmail.com>
2021-07-06 00:45:49 +0800

    rel

--- src/repeat.sh
+++ src/repeat.sh
@@ -1,3 +1,4 @@
 /root/anaconda3/envs/new_env/bin/python3.6 main.py --gpu 3 --mimethod mine --method nmp --task probe --batch_size 2048 --repeat 15 --nonlinear elu --hidden_size 128 --lr 1e-3 --milr 5e-4 --epoch 1000 --mode sample
 /root/anaconda3/envs/new_env/bin/python3.6 main.py --gpu 2 --mimethod mine --method graphsage --task probe --batch_size 2048 --repeat 15 --nonlinear elu --hidden_size 64 --lr 3e-5 --milr 5e-4 --epoch 1000 --mode sample
 /root/anaconda3/envs/new_env/bin/python3.6 main.py --gpu 3 --mimethod mine --method deepwalk --task probe --batch_size 2048 --repeat 15 --nonlinear elu --hidden_size 64 --lr 3e-3 --milr 5e-4 --epoch 1000 --mode sample
+/root/anaconda3/envs/new_env/bin/python3.6 main.py --gpu 3 --mimethod mine --method deepwalk --task probe --batch_size 2048 --repeat 15 --nonlinear elu --hidden_size 64 --lr 3e-3 --milr 5e-4 --epoch 1000 --mode sample
